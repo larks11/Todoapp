c83a89d0514a22c2ff392cb7b29fc3abcadefa22
larks11 <lark.alunan@gmail.com>
2025-12-02 22:47:44 +0800

    Fix base path for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Todo-app/',
+  base: './',        // <-- change this from '/Todo-app/' to './'
   plugins: [react()],
   server: {
     port: 3000,
